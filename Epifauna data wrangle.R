3f70720e63787a486123f928966964dc587472c8
kskoehler10 <143293575+kskoehler10@users.noreply.github.com>
2023-11-04 16:59:53 -0700

    Add files via upload

--- Epifauna data wrangle.R
+++ Epifauna data wrangle.R
@@ -1,15 +1,16 @@
 #####
 ##### Karl's attempt at wrangling the data for subsequent analyses
 #####
+library(dplyr)
 
 
 ### Load most recent epifauna data
 ### See Carmen's code and readme on google drive for the detailed formation of this dataset. 
 
 ### Looking at carmens new data set posted 8/24/2023 on google drive
-taxa1 <- read.csv("C:/Users/Karl/Desktop/Grad School/Lab/eel grass/eg wasting epifauna/8262023/20230824_allepifauna_wide_macrophyte.csv")
-nrow(taxa1) #1006
-ncol(taxa1) #333
+taxa1 <- read.csv("C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/20230824_allepifauna_wide_macrophyte.csv")
+nrow(taxa1) #1017
+ncol(taxa1) #345
 
 
 ## First need to deal with some duplicate row names. I found the appropriate row names from data sheet scans on Box and fixed them.
@@ -39,19 +40,20 @@ ncol(taxa1)## 344
 
 ### Remove species we didn't target that we don't want in nay analyses (ostracods, cumaceans, fish, etc)
 ##also remove nonsense columns and empty taxa
-taxa1<-taxa1%>% select(-c(Allorchestes.sp.,bivalve.D,Cumella.vulgaris,EMPTY,fish,Fish.larva,Neanthes.sp.,Nippoleucon.hinumensis,Pipefish))
+## Used the following before but it stopped working for me#  taxa1<-taxa1%>% select(-c(Allorchestes.sp.,bivalve.D,Cumella.vulgaris,EMPTY,fish,Fish.larva,Neanthes.sp.,Nippoleucon.hinumensis,Pipefish))
+#### taxa1<-taxa1[-c(Allorchestes.sp.,bivalve.D,Cumella.vulgaris,EMPTY,fish,Fish.larva,Neanthes.sp.,Nippoleucon.hinumensis,Pipefish)]
 ###I have removed 9 columns so far but there are probably others to get rid of
-
+taxa1<-taxa1[ ,!names(taxa1) %in% c("Allorchestes.sp.","bivalve.D","Cumella.vulgaris","EMPTY","fish","Fish.larva","Neanthes.sp.","Nippoleucon.hinumensis","Pipefish")]
 
 ###Rename any taxa whose nomenclature is not current with WORMS####
-## Carmen already fixed this in teh 8/24/2023 version of data
+
+## Carmen already fixed this in the 8/24/2023 version of data
 ###I did not do a thorough search, just happened to find these
 #taxa1<-taxa1 %>%rename(Mitrella.gausapata = Astyris.gausapata,Angustassiminea.californica=Assiminea.californica,Megasyllis.nipponica=Typosyllis.nipponica)
-
 ### I will also rename some things that we've putatively improved the ID's of since data was collected.
 ## There were more of these to do in previous versions of dataset
-taxa1<-taxa1 %>%
-  rename(Nassarius.tiarula=gastropod.N) 
+#taxa1<-taxa1 %>%rename(Nassarius.tiarula=gastropod.N) 
+
 ## Combine some columns of unknowns with their updated and preexisting identities
 taxa1$Zygonemertes.sp.<-taxa1$Zygonemertes.sp.+taxa1$green.worm.thing
 taxa1<-taxa1[ ,!names(taxa1) %in% c("green.worm.thing")]
@@ -121,17 +123,29 @@ dim(taxa.only.transect) ## 508,304
 
 ############################################
 ##### This data frame contains only taxa observed at the transect level per year and is suitable for calculating diversity indices #####
-write.csv(taxa.only.transect,"C:/Users/Karl/Desktop/Grad School/Lab/eel grass/eg wasting epifauna/8262023/taxa.only.transect.csv")
+write.csv(taxa.only.transect,"C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/taxa.only.transectv2.csv")
+
+## Create transformed version of taxa only
+sqrt.taxa.only.transect<-sqrt(taxa.only.transect)
 
 ### If we want to standardize abundance per sample by eelgrasss biomass use this data
 taxa.only.transect.biomass<-as.data.frame(as.matrix(taxa.only.transect)/as.vector(taxa.transect1$macrophyte_wet_mass_g))
-write.csv(taxa.only.transect.biomass,"C:/Users/Karl/Desktop/Grad School/Lab/eel grass/eg wasting epifauna/8262023/taxa.only.transect.biomass.csv")
+write.csv(taxa.only.transect.biomass,"C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/taxa.only.transect.biomass.csv")
 
 
 ##### Now we can calculate diversity indices and insert into transect level data####
 taxa.transect1$simpsons<-diversity(taxa.only.transect,index="simpson")
 taxa.transect1$shannon<-diversity(taxa.only.transect,index="shannon")
-taxa.transect1$invsimpsons<-diversity(taxa.only.transect,index="invsimpson")
+
+## Using sqrt transformed abundances
+taxa.transect1$simpsons.sqrt<-diversity(sqrt.taxa.only.transect,index="simpson")
+taxa.transect1$shannon.sqrt<-diversity(sqrt.taxa.only.transect,index="shannon")
+
+##Abundance
+taxa.transect1$abundance<-rowSums(taxa.only.transect)
+
+##Richness
+taxa.transect1$richness<-apply(taxa.only.transect>0,1,sum)
 
 
 ########################################
@@ -139,4 +153,196 @@ taxa.transect1$invsimpsons<-diversity(taxa.only.transect,index="invsimpson")
 ########################################
 
 #### This data can be used for univariate community level statistics
-write.csv(taxa.transect1,"C:/Users/Karl/Desktop/Grad School/Lab/eel grass/eg wasting epifauna/8262023/taxa.transect1.csv")
+write.csv(taxa.transect1,"C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/taxa.transect1.csv")
+
+
+
+
+######################################################
+####### Environmental data ######
+######################################
+
+env<-read.csv("C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/EGWD_transect_metrics_20230925.csv",header=TRUE,row.names=56)
+
+## Keep only rows that are represnted in epifauna data
+rmlist3<-as.list(row.names(taxa.only.transect))
+length(rmlist3)
+dim(env)
+env<-env[ (row.names(env) %in% rmlist3),]
+dim(env)
+
+## Make a data frame containing env data and some univariate metrics we might want to model
+uni1<-cbind(env,taxa.transect1[c(1:3,308:339)])
+
+
+
+
+#####READY FOR BASIC UNIVARIATE STATS#####
+### At this point we should be able to calculate accurate richness and abundance measures
+## This assumes that all taxa columns remaining are not redundant, 
+## or if they are (e.x. Ericthonius sp. and Ericthonius brasiliensis) that the redundant taxa are
+## never both encountered in the same sample ##
+
+## We might want to use inverse Simpson's for diveristy to downweight the numerous rare taxa whose IDs
+## are often uncertain. (Boye et al. 2017; Hill 1973)
+
+################################
+###############################
+
+
+#######PREPARE DATA FOR MULTIVARIATE ANALYSES###########
+
+#### First, we need to choose some sort of cutoff threshold to remove the very rare taxa from our data.
+## This will reduce noise in our community composition data and also conveniently get rid of most of 
+## the taxa which we do not have good IDs for.
+
+#### Many studies do not employ a cutoff value. That being said, many of these studies have lower
+## diversity or only ID to the family level. We have a lot of unknown signletons and very rare taxa 
+## which I do not think we have enough information about to make intelligible conclusions so I
+## think we need some cutoff.
+
+### There are different thresholds used in the literature.
+
+## Murphy et al. (2021) exclude taxa with less than 5 individuals across the study, unidentified taxa,
+## and taxa not strictly considered epifauna.
+
+## Whippo et al. 2018 got rid of all taxa they did not consider to be strictly "epifaunal"
+
+## The PRIMER manual by Clarke et al. suggests using including only taxa that make up at least 
+## (q > 1%-3%) of any single sample in the study. See Field et al. 1982 as well.
+
+## Transforming the data will also affect the weight of rare species in subsequent analyses.
+## Square root and log transformations downweight highly abundant taxa and elevate rare taxa.
+## Hellinger transformation reduces the weight of rare taxa
+## (Boye et al. 2017; Legendre and Gallagher 2001)
+
+##### I will use a q value of 3% below
+dim(taxa.only.transect)
+
+abund.vec<-as.vector(taxa.transect1$abundance)
+taxamat1<-as.matrix(taxa.only.transect)
+length(abund.vec)
+dim(taxamat1)
+
+### produce a matrix of the percentage each species makes up of each sample it is found in.
+taxamat2<-taxamat1/(abund.vec)
+taxamat2[is.na(taxamat2)] <- 0
+dim(taxamat2)
+### Find the maximum proportion it makes up of any sample in the study
+clmx1<-apply(taxamat2,MARGIN=2,max)
+glimpse(clmx1)
+### Define q value here.
+clmx2<-clmx1[clmx1<.03]
+glimpse(clmx2)
+### create list of taxa to be included
+rmlist1<-names(clmx2)
+length(rmlist1)
+dim(taxa.only.transect)
+### create new data frame with only taxa above threshold
+taxa.sub1<-taxa.only.transect[ , !(names(taxa.only.transect) %in% rmlist1)]
+
+## How much does this cut out?
+dim(taxa.sub1)  ## Leaves us with 144 taxa (removes 157 taxa)
+taxa.removed<-taxa.only.transect[ , (names(taxa.only.transect) %in% rmlist1)]
+dim(taxa.removed)
+total.removed<-sum(rowSums(taxa.removed)) ##only 836 individuals removed from data
+total.removed
+total.removed/sum(taxa.transect1$abundance) ## Which amounts to less than 1 percent of total abundance
+
+
+write.csv(taxa.sub1,"C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/taxa.sub1.csv")
+
+
+
+#### To compare community composition between samples we need to make sure that there are
+## no duplicate taxa represented by different names in different regions.
+
+#### For example  Ericthonius sp. (as they are called in BB) and Ericthonius brasiliensis (as they are called elsewhere) 
+## should probably be treated as synonymous and combined into a single column.
+#### For situations where there is one species level ID in some sites that corresponds to a genus level
+## ID in other sites, I will use the genus level ID for all cases.
+
+#### For situations where there are columns for a genus and multiple species within that genus
+## I will combine into one genus level ".spp" column
+
+spe2<-taxa.sub1
+
+spe2$Acteocina.sp.<-spe2$Acteocina.sp.+spe2$Acteocina.inculta
+spe2<-spe2[ ,!names(spe2) %in% c("Acteocina.inculta")]
+
+spe2$Ampithoe.spp<-spe2$Ampithoe.sp.+spe2$Ampithoe.valida+spe2$Ampithoe.lacertosa
+rm.ampithoe<-c("Ampithoe.sp.","Ampithoe.valida","Ampithoe.lacertosa")
+spe2<-spe2[ ,!names(spe2) %in% rm.ampithoe]
+
+#Also conflict with Aoridae
+spe2$Aoroides.spp<-spe2$Aoroides.sp.+spe2$Aoroides.columbiae
+spe2<-spe2[ ,!names(spe2) %in% c("Aoroides.sp.","Aoroides.columbiae")]
+
+spe2$Calliopius.spp<-spe2$Calliopius.sp.+spe2$Calliopius.carinatus
+spe2<-spe2[ ,!names(spe2) %in% c("Calliopius.carinatus","Calliopius.sp.")]
+
+spe2$Cancer.spp<-spe2$Cancer.sp.+spe2$Cancer.branneri
+spe2<-spe2[ ,!names(spe2) %in% c("Cancer.branneri","Cancer.sp.")]
+
+spe2$Caprella.spp<-spe2$Caprella.sp.+spe2$Caprella.californica+spe2$Caprella.laeviuscula
+spe2<-spe2[ ,!names(spe2) %in% c("Caprella.californica","Caprella.laeviuscula","Caprella.sp.")]
+
+#Went with E.brasiliensis because I know it is common throughout the range
+spe2$Ericthonius.brasiliensis<-spe2$Ericthonius.brasiliensis+spe2$Ericthonius.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Ericthonius.sp.")]
+
+spe2$Exogone.spp<-spe2$Exogone.lourei+spe2$Exogone.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Exogone.lourei","Exogone.sp.")]
+
+spe2$Haminoea.japonica
+spe2$Haminoea.spp<-spe2$Haminoea.sp.+spe2$Haminoea.vesicula
+spe2<-spe2[ ,!names(spe2) %in% c("Haminoea.sp.","Haminoea.vesicula")]
+
+spe2$Heptacarpus.sp.<-spe2$Heptacarpus.sp.+spe2$Heptacarpus.paludicola
+spe2<-spe2[ ,!names(spe2) %in% c("Heptacarpus.paludicola")]
+
+spe2$Hippolyte.sp.<-spe2$Hippolyte.sp.+spe2$Hippolyte.californiensis
+spe2<-spe2[ ,!names(spe2) %in% c("Hippolyte.californiensis")]
+
+spe2$Idotea.sp.<-spe2$Idotea.sp.+spe2$Idotea.ochotensis
+spe2<-spe2[ ,!names(spe2) %in% c("Idotea.ochotensis")]
+
+spe2$Lacuna.spp<-spe2$Lacuna.sp.+spe2$Lacuna.variegata+spe2$Lacuna.vincta+spe2$Lacuna.marmorata
+spe2<-spe2[ ,!names(spe2) %in% c("Lacuna.sp.","Lacuna.variegata","Lacuna.vincta","Lacuna.marmorata")]
+
+spe2$Monocorophium.spp<-spe2$Monocorophium.acherusicum+spe2$Monocorophium.insidiosum+spe2$Monocorophium.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Monocorophium.acherusicum","Monocorophium.insidiosum","Monocorophium.sp.")]
+
+
+##What to do about "Nereid" and "Nereis sp." along with "Platynereis.bicanaliculata" etc?
+
+spe2$Pentidotea.spp<-spe2$Pentidotea.resecata+spe2$Pentidotea.schmitti+spe2$Pentidotea.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Pentidotea.resecata","Pentidotea.schmitti","Pentidotea.sp.")]
+
+spe2$Photis.spp<-spe2$Photis.brevipes+spe2$Photis.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Photis.brevipes","Photis.sp.")]
+
+spe2$Podocerus.spp<-spe2$Podocerus.fulanus+spe2$Podocerus.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Podocerus.fulanus","Podocerus.sp.")]
+
+spe2$Polynoid.spp<-spe2$Polynoid.A+spe2$Polynoid.C+spe2$Polynoidae+spe2$Polynoid..unknown.
+spe2<-spe2[ ,!names(spe2) %in% c("Polynoid.A","Polynoid.C","Polynoidae","Polynoid..unknown.")]
+
+spe2$Phyllodocidae.spp<-spe2$Phyllodocidae+spe2$Phyllodocidae.B+spe2$Phyllodocidae.C
+spe2<-spe2[ ,!names(spe2) %in% c("Phyllodocidae","Phyllodocidae.B","Phyllodocidae.C")]
+
+spe2$Pontogeneia.spp<-spe2$Pontogeneia.rostrata+spe2$Pontogeneia.sp.
+spe2<-spe2[ ,!names(spe2) %in% c("Pontogeneia.rostrata","Pontogeneia.sp.")]
+
+
+## I will also remove all singletons. There are 7 in total
+singletons<-names(which (colSums(spe2) == 1))
+taxa.multi<-spe2[ ,!names(spe2) %in% singletons]
+dim(taxa.multi)
+sum(taxa.multi)
+
+##  Now we have a dataset which is ideal for multivariate comparisons of community composition
+## Total of 135075 individuals representing 111 taxa 
+write.csv(taxa.multi,"C:/Users/kskoe/OneDrive/Desktop/wasting disease 10-23/taxa.multi.csv")
+
+
